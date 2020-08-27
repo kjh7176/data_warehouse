4a14698546e53d865379246791d6940ceeae9541
kjh7176 <5wngus@gmail.com>
2020-08-27 19:59:34 +0000

    fix: complete functions

--- iac.py
+++ iac.py
@@ -2,23 +2,38 @@ import configparser
 import boto3
 import json
 from botocore.exceptions import ClientError
-from asyncio import sleep
+from time import sleep
 
 config = configparser.ConfigParser()
 config.read('dwh.cfg')
 
 KEY = config.get('ACCESS', 'KEY')
 SECRET = config.get('ACCESS', 'SECRET')
-ROLE_NAME = config.get('IAM_ROLE', 'NAME')
+IAM_ROLE = config.get('IAM_ROLE', 'ARN')
+
+DWH_CLUSTER_TYPE = config.get('DWH', 'DWH_CLUSTER_TYPE')
+DWH_NUM_NODES = config.get('DWH', 'DWH_NUM_NODES')
+DWH_NODE_TYPE = config.get('DWH', 'DWH_NODE_TYPE')
+CLUSTER_IDENTIFIER = config.get('DWH', 'DWH_CLUSTER_IDENTIFIER')
+DB_NAME = config.get('CLUSTER', 'DB_NAME')
+DB_USER = config.get('CLUSTER', 'DB_USER')
+DB_PW = config.get('CLUSTER', 'DB_PASSWORD')
+DB_PORT = config.get('CLUSTER', 'DB_PORT')
+
+# create client for Redshift
+redshift = boto3.client('redshift',
+                        region_name = "us-west-2",
+                        aws_access_key_id = KEY,
+                        aws_secret_access_key = SECRET)
+"""
+# create client for IAM
+iam = boto3.client('iam',
+                   region_name = "us-west-2",
+                   aws_access_key_id = KEY,
+                   aws_secret_access_key = SECRET)
 
 def create_iam_role():
-    # create client for IAM
-    iam = boto3.client('iam',
-        region_name = "us-west-2",
-        aws_access_key_id = KEY,
-        aws_secret_access_key = SECRET)
     
-
     try:
         # create the IAM role
         iamRole = iam.create_role(
@@ -46,29 +61,14 @@ def create_iam_role():
         PolicyArn="arn:aws:iam::aws:policy/AmazonS3ReadONlyAccess")['ResponseMetadata']['HTTPStatusCode']
         
         arn = iam.get_role(RoleName=ROLE_NAME)['Role']['Arn']
-        
+    
+    config.set('IAM_ROLE', 'ARN', arn)
     # return the IAM role ARN
     return arn
     
-
+"""
 
 def create_cluster():
-
-    ARN = create_iam_role()
-    DWH_CLUSTER_TYPE = config.get('DWH', 'DWH_CLUSTER_TYPE')
-    DWH_NUM_NODES = config.get('DWH', 'DWH_NUM_NODES')
-    DWH_NODE_TYPE = config.get('DWH', 'DWH_NODE_TYPE')
-    CLUSTER_IDENTIFIER = config.get('DWH', 'DWH_CLUSTER_IDENTIFIER')
-    DB_NAME = config.get('CLUSTER', 'DB_NAME')
-    DB_USER = config.get('CLUSTER', 'DB_USER')
-    DB_PW = config.get('CLUSTER', 'DB_PASSWORD')
-    
-    # create client for Redshift
-    redshift = boto3.client('redshift',
-        region_name = "us-west-2",
-        aws_access_key_id = KEY,
-        aws_secret_access_key = SECRET)
-    
     try:
         # create cluster
         response = redshift.create_cluster(
@@ -81,7 +81,7 @@ def create_cluster():
             MasterUsername = DB_USER,
             MasterUserPassword = DB_PW,
             
-            IamRoles = [ARN]
+            IamRoles = [IAM_ROLE]
         )
         
     except Exception as e:
@@ -93,24 +93,25 @@ def create_cluster():
     else:
         print("Created cluster!")
         
-    cluster_props = redshift.describe_clusters(ClusterIdentifier=CLUSTER_IDENTIFIER)['Clusters'][0]
-    set_security_group(cluster_props['VpcId'])
-    endpoint = get_endpoint(cluster_props)
+    set_security_group(redshift.describe_clusters(ClusterIdentifier=CLUSTER_IDENTIFIER)['Clusters'][0]['VpcId'])
+    endpoint = get_endpoint()
     
     return endpoint
 
-def get_endpoint(cluster_props):
-    while(cluster_props["ClusterStatus"] == "modifying"):
-        sleep(10)
-        
-    if cluster_props["ClusterStatus"] == "available":
-        return cluster_props['Endpoint']['Address']
-    else:
+def get_endpoint():
+    try:
+        while(redshift.describe_clusters(ClusterIdentifier=CLUSTER_IDENTIFIER)['Clusters'][0]["ClusterStatus"] == "creating"):
+            print("creating")
+            sleep(30)   
+
+        if redshift.describe_clusters(ClusterIdentifier=CLUSTER_IDENTIFIER)['Clusters'][0]["ClusterStatus"] == "available":
+            return redshift.describe_clusters(ClusterIdentifier=CLUSTER_IDENTIFIER)['Clusters'][0]['Endpoint']['Address']
+        else:
+            return None
+    except Exception as e:
         return None
-        
 
 def set_security_group(vpc_id):
-    DB_PORT = config.get('CLUSTER', 'DB_PORT')
     
     # create client for ec2
     ec2 = boto3.resource('ec2',
@@ -130,13 +131,19 @@ def set_security_group(vpc_id):
         )
     except Exception as e:
         if e.response['Error']['Code'] == 'InvalidPermission.Duplicate':
-            print("Permission already exists!")
+            pass
         else:
             print(e)
 
 def delete_cluster():
-    pass
+    redshift.delete_cluster(ClusterIdentifier=CLUSTER_IDENTIFIER, 
+                            SkipFinalClusterSnapshot=True)
+    
+    try:
+        while(redshift.describe_clusters(ClusterIdentifier=CLUSTER_IDENTIFIER)['Clusters'][0]["ClusterStatus"] == "deleting"):
+            print("deleting")
+            sleep(30)
+    except Exception as e:
+        if e.response['Error']['Code'] == 'ClusterNotFoundFault':
+            print("Deleted cluster!")
 
-
-if __name__ == "__main__":
-    print(create_cluster())
